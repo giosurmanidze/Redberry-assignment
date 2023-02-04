375111696ddfc6ce2880122d3c3fdf89026e5399
giosuranidze <octopusoctopus796@gmail.com>
2023-02-04 12:30:20 +0400

    created blank resume component

---
+++ src/components/Resume.jsx
@@ -0,0 +1,12 @@
+import "./styles/Resume.css";
+import logo from "../assets/images/LOGO-12 1.png";
+
+const Resume = () => {
+  return (
+    <div className="resume__paper">
+      <img src={logo} alt="logo" className="cv-logo" />
+    </div>
+  );
+};
+
+export default Resume;
