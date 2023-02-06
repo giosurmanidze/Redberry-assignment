115bebb6bbec3f33cc77fdc56d0574e07406ce8f
giosuranidze <giorgisurmanidze917@gmail.com>
2023-02-06 17:46:35 +0400

    I validated the general page with its own error messages and icons.

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,10 +1,27 @@
 import "./styles/Resume.css";
-import logo from "../assets/images/LOGO-12 1.png";
+import LOGO from "../assets/images/LOGO-12 1.png";
+import EMAIL_ICON from '../assets/images/email-icon.png'
+import PHONE_ICON from '../assets/images/phone-icon.png'
 
-const Resume = () => {
+const Resume = ({data,imgUrl}) => {
+
   return (
     <div className="resume__paper">
-      <img src={logo} alt="logo" className="cv-logo" />
+      <div className="header">
+        <div className="header__infos">
+          <div>
+            <h1>{data?.name} {data?.surname}</h1>
+            <p className="email__detail">{data?.email && <img  src={EMAIL_ICON}/>}{data?.email}</p>
+            <p className="phone__detail">{data?.phone_number && <img  src={PHONE_ICON}/>}{data?.phone_number}</p>
+          </div>
+          <div className="about__me--info">
+            {data?.about_me &&  <h3>ჩემ შესახებ</h3>}
+            <p>{data?.about_me}</p>
+          </div>
+        </div>
+        <img className="person__image" src={imgUrl}/>
+      </div>
+      <img src={LOGO} alt="logo" className="cv-logo" />
     </div>
   );
 };
