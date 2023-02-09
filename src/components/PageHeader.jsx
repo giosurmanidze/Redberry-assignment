69d068d85eeebf6aa3d7915057eca65839d85d24
giosuranidze <giorgisurmanidze917@gmail.com>
2023-02-09 22:33:16 +0400

    updated header component

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,14 +1,27 @@
+import { Link } from "react-router-dom";
+import arrowIcon from "../assets/images/Vector.png";
+
 const PageHeader = ({ title, status }) => {
+  // IF ARROW ON THE TOP LEFT IS CLICKED REFRESH ALL SAVED DATA
+  const backAndRefresh = () => {
+    sessionStorage.clear();
+  };
   return (
-    <>
+    <div className="header__details">
+      <div className="arrow__icon">
+        <Link to="/" onClick={backAndRefresh}>
+          <img src={arrowIcon} alt="arrow" />
+        </Link>
+      </div>
       <div className="general__screen--header">
+        
         <div className="top">
           <h3>{title}</h3>
           <p className="general__screen--status">{status}/3</p>
         </div>
         <div className="general__screen--line" />
       </div>
-    </>
+    </div>
   );
 };
 
