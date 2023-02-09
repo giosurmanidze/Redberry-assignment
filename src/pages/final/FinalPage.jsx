952389419cf7e077e25afed296e9421685f39921
giosuranidze <octopusoctopus796@gmail.com>
2023-02-09 20:00:52 +0400

    Completed the overall design of the resume page. I also styled the scrollbar and added an animation to the success popup.

--- src/pages/final/FinalPage.jsx
+++ src/pages/final/FinalPage.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import Resume from "../../components/Resume";
+import arrowIcon from '../../assets/images/down-arrow.png'
+import { Link } from "react-router-dom";
+import "./style/styles.css";
 
 const FinalPage = () => {
-  return (
-    <div>FinalPage</div>
-  )
-}
 
-export default FinalPage
+
+  // IF ARROW ON THE TOP LEFT IS CLICKED REFRESH ALL SAVED DATA
+  const backAndRefresh = () => {
+    sessionStorage.clear();
+  };
+  return (
+    <div className="resume__screen">
+      <div className="arrow__icon">
+        <Link to="/" onClick={backAndRefresh}>
+          <img src={arrowIcon} alt="arrow" />
+        </Link>
+      </div>
+      <div className="cv__info">
+        <Resume
+          data={JSON.parse(sessionStorage.getItem("inputData"))}
+          imgUrl={JSON.parse(sessionStorage.getItem("imgUrl"))}
+          expData={JSON.parse(sessionStorage.getItem("experienceData"))}
+          eduData={JSON.parse(sessionStorage.getItem("educationData"))}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FinalPage;
